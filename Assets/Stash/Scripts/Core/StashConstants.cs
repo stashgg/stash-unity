6733735b162f7865af14c58051d71bb19befce73
Ondrej Rehacek <Ondrejrehacek@gmail.com>
2024-03-12 12:02:38 +0100

    URL Change for the API

--- Assets/Stash/Scripts/Core/StashConstants.cs
+++ Assets/Stash/Scripts/Core/StashConstants.cs
@@ -2,7 +2,7 @@ namespace Stash.Core
 {
     public class StashConstants
     {
-        public const string APIRootURL = "https://api-rest-f57w5ea4ya-uc.a.run.app";
+        public const string APIRootURL = "https://test-api.stash.gg/";
         public const string TestRootURL = "https://stash.requestcatcher.com";
         public const string LinkAccount = "/sdk/link_code/link";
         public const string LnkGameCenter = "/sdk/link_code/link_game_center";
