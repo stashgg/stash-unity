a3724d3ec64255ea3076c7909a024cc511917411
Ondrej Rehacek <Ondrejrehacek@gmail.com>
2024-05-27 23:29:13 +0200

    Added CustomLogin

--- Assets/Stash/Scripts/Core/StashConstants.cs
+++ Assets/Stash/Scripts/Core/StashConstants.cs
@@ -6,8 +6,9 @@ namespace Stash.Core
         public const string RootUrlTest = "https://test-api.stash.gg";
         
         public const string LinkAccount = "/sdk/link_code/link";
-        public const string LinkCustomAccount = "/sdk/custom_login/approve";
+        public const string CustomLogin = "/sdk/custom_login/approve_with_jwt";
         public const string LinkAppleGameCenter = "/sdk/link_code/link_apple_game_center";
         public const string LinkGooglePlayGames = "/sdk/link_code/link_google_play";
+        
     }
 }
